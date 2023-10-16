af72be2c0ab0afff026985aaefae5d7a01da29d4
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2023-10-16 23:14:49 +0800

    Revert "Fix layout"
    This reverts commit 55e5deb696287c9dd668a7b09dab60f16c5ba4e3.

--- client/real_estate_portal/src/components/properties/PropertyDetail.js
+++ client/real_estate_portal/src/components/properties/PropertyDetail.js
@@ -3,12 +3,10 @@ import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
-import { BsBook } from 'react-icons/bs';
 import '../../styles/PropertyDetail.css';
 
 
 const PropertyDetail = ({ object, investors, amenity }) => {
-  const [showInstructions, setShowInstructions] = useState(false);
   const [index, setIndex] = useState(0);
   const [highlightedIndex, setHighlightedIndex] = useState(0);
   console.log(object)
@@ -17,10 +15,6 @@ const PropertyDetail = ({ object, investors, amenity }) => {
     // Update the highlighted index when the index changes
     setHighlightedIndex(index);
   }, [index]);
-
-  const toggleInstructions = () => {
-    setShowInstructions(!showInstructions);
-  };
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
@@ -68,36 +62,33 @@ const PropertyDetail = ({ object, investors, amenity }) => {
             </div>
           </div>
         </div>
-        <div className="col-md-8 mt-3">
-          <div className="">
-            <strong style={{ fontSize: '1.5rem' }}>{object.name}</strong>
-            <small className="badge bg-secondary text-light rounded-0 mx-5">{object.type.category_name}</small>
-            <br/>
+        <div className="col-md-8">
+          <div className="d-flex">
+            <h3 style={{ fontSize: '1.5rem', lineHeight: '2.5rem', fontWeight: '700' }}>{object.name}</h3>
+            <p className="badge bg-secondary text-light rounded-0 mx-5">{object.type.category_name}</p>
+          </div>
+          <p className="pt-0">
             <span className="mx-0"><small>BED: {object.bed || 0}</small></span>
             <span className="mx-2"><small>BATH: {object.bath || 0}</small></span>
             <span className="mx-2"><small>SQ FT: {object.squareFeet || 0}</small></span>
             <span className="mx-2"><small>YEAR BUILT: {object.buildYear || 'N/A'}</small></span>
-          </div>
+          </p>
           <div className="row mt-5">
               <div className="col-md-3">
                 <span className="underline text-secondary">Investors</span>
-                <br/>
-                <strong>{investors || 0}</strong>
+                <p>{investors || 0}</p>
               </div>
               <div className='col-md-3'>
                 <span className="underline text-secondary">Property Leverage</span>
-                <br/>
-                <strong>0%</strong>
+                <p>0%</p>
               </div>
               <div className='col-md-3'>
                 <span className="underline text-secondary">Purchase Price</span>
-                <br/>
-                <strong>${object.purchasePrice || 0}</strong>
+                <p>${object.purchasePrice || 0}</p>
               </div>
               <div className='col-md-3'>
                 <span className="underline text-secondary">Monthly Rent</span>
-                <br/>
-                <strong>${object.projectedRent || 0}</strong>
+                <p>${object.projectedRent || 0}</p>
               </div>
               <div>           
             </div>
@@ -118,49 +109,8 @@ const PropertyDetail = ({ object, investors, amenity }) => {
             )}
           </div> */}
           <strong>Address: {object.address1}</strong>
-
-          <div style={{ backgroundColor: '#f5f5f5' }} className="tenant-process text-dark p-3 mt-5" onClick={toggleInstructions}>
-            <div className="text-dark p-3 align-items-center">
-              <BsBook className="mr-2 pr-2"/>
-              <strong className='pl-2 ml-2' style={{ fontSize: '1.2rem' }}> Tenant Leasing Process</strong>
-              {showInstructions && (
-              <p className="instructions my-2 pt-1 text-dark" style={{ fontSize: '1rem'}}>
-                  Preparing a home for the rental market is a multi-tiered process that begins as soon as the property is acquired. Our experienced investment team works closely with the local property managers to get it show-ready, determine optimal market rent, take high-quality photos and videos, market the rental through various online and offline channels, screen and thoroughly vet tenant applications, and finalize the lease terms. Below you can find more information regarding the current rental status, the expected timeline, and the rental income.
-              </p>
-            )}
-            </div>
-          </div>
-
-          <div className='row py-5'>
-              <div className='col-md-6'>
-                <span className="underline text-secondary">Rental Status</span>
-                <br/>
-                <strong>Approved application</strong>
-              </div>
-              <div className='col-md-6'>
-                <span className="underline text-secondary">Anticipated First Dividend Date</span>
-                <br/>
-                <strong>September 2024</strong>
-              </div>
-          </div>
-
-          <div>
-            <span className='text-secondary'>
-              Timeline to Rent
-            </span>
-            <br/>
-            <span>
-              The time needed to sign the first tenant lease is variable and can depend on several factors including seasonality and whether the property requires any sort of renovation. Historically the average time to lease has been 45 days and has ranged from a minimum of 7 days to over 90 days.
-            </span>
-          </div>
-
-          <div style={{ backgroundColor: '#f5f5f5' }} className="text-dark p-4 mt-5">
-            <strong className='pl-2 ml-2' style={{ fontSize: '1.2rem' }}>
-              Investment Strategy Hypothetical Returns
-            </strong>
-          </div>
         </div>
-        <div className='col-md-4 mt-3'>
+        <div className='col-md-4 mt-1'>
           <Card style={{ width: '24rem',  borderRadius: '0', borderBottom: 'solid 15px gray' }}>
             <ListGroup variant="flush">
               <ListGroup.Item className='p-3'>
